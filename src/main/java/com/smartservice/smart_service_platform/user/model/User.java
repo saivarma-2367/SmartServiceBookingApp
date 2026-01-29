41e56bd98d06178734a6cc988bbd1381d5b3d6ae
“saivarma-2367” <ssiravur@gitam.in>
2026-01-29 09:07:42 +0530

    User created successfully

---
+++ src/main/java/com/smartservice/smart_service_platform/user/model/User.java
@@ -0,0 +1,23 @@
+package com.smartservice.smart_service_platform.user.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean enabled = true;
+}
