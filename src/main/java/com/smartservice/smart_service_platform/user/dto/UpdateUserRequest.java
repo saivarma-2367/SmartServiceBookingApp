49fc4af9ea7f3806038d83e7ad1c728f2652c92c
“saivarma-2367” <ssiravur@gitam.in>
2026-01-29 14:22:23 +0530

    auth implemented successfully

---
+++ src/main/java/com/smartservice/smart_service_platform/user/dto/UpdateUserRequest.java
@@ -0,0 +1,4 @@
+package com.smartservice.smart_service_platform.user.dto;
+
+public record UpdateUserRequest(String name) {
+}
---
+++ src/main/java/com/smartservice/smart_service_platform/user/dto/UserResponse.java
@@ -0,0 +1,4 @@
+package com.smartservice.smart_service_platform.user.dto;
+
+public record UserResponse(Long id,String name,String email,String role) {
+}
