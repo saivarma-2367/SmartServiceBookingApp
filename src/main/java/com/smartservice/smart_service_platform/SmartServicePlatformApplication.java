2add623dd22f5ccb57a507eac309a168efa9358c
“saivarma-2367” <ssiravur@gitam.in>
2026-01-29 09:00:43 +0530

    Initial commit

---
+++ src/main/java/com/smartservice/smart_service_platform/SmartServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.smartservice.smart_service_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServicePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServicePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartservice/smart_service_platform/SmartServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartservice.smart_service_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartServicePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
